041cd32b7129b362d43ba04e459be1050d621e4f
CyclonaGowen <cgowen@dialecticapps.com>
2021-08-14 13:11:10 -0500

    data files and css fix

--- src/Review.js
+++ src/Review.js
@@ -1,31 +1,36 @@
 import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-
 const Review = () => {
-  const [index, setIndex] = useState(3);
-  const{name, breed, image, text, owner} = people[index];
-  return <article className="review">
-    <div className="img-container">
-      <img src={image} alt={name} className="person-img" />
-      <span className="quote-icon">
-        <FaQuoteRight />
+  const [index, setIndex] = useState(0);
+  const { name, breed, image, text, owner } = people[index];
+
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
         </span>
-    </div>
-    <h4 className="author">
-    <p className="breed">
-      {breed}
-    </p>
-      {name}
-    </h4>
-    
-    <p className="info">
-      {text}
-    </p>
-    <p className="info">
-      <lable>Pet Parent: {owner}</lable>
-    </p>
-  </article>;
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='breed'>{breed}</p>
+      <p className='info'>{text}</p>
+      <p className='owner'>{owner}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>
+        surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
